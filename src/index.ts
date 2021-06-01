6bcf5bc1e725a9077a88c76db858b3b9aba802a3
Francesco Virga <francescod.virga@gmail.com>
2021-06-01 16:13:54 -0400

    Test

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ const lintStaged = require('lint-staged');
       }
     });
 
-    console.log(success ? "Format and lint was successful 🏀" : "Linting failed!");
+    console.log(success ? "Format and lint was successful 🏀" : 'Linting failed!');
   } catch (err) {
     // Failed to load configuration
     throw err;
