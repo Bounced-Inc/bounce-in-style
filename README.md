ce9abd28208263474b6c4fe6d2dabfd23bbe4900
Francesco Virga <francescod.virga@gmail.com>
2021-06-02 15:07:48 -0400

    Add cli option support

--- README.md
+++ README.md
@@ -4,17 +4,9 @@ The Bounce style guide, formatter, and commit validator.
 
 # Goal
 
-- a single source of truth for all linting, formatting, commit message validation, changelog generator, semantic versioning
-- minimal config required
-- compatible with all Bounce Typescript projects
-- separate config for react / react-native
-
-## Progres and whats next
-
-- test current setup in terms of linting / formatting
-- try adding airbnb extends (optional, or later)
-- setup cli to run: `bis { --staged | --all } { --react }`
-- setup commit validator & changelog generator
+- Single source of truth for all linting, formatting, commit message validation, changelog generator, semantic versioning
+- Minimal config
+- Compatible with all Bounce Typescript projects (Express, React, React Native)
 
 # Instructions
 
@@ -34,7 +26,22 @@ npm install bounce-in-style -D
 - Add git hook with Husky
 
 ```bash
-npx husky add .husky/pre-commit "npx bis"
+# react/react-native projects
+npx husky add .husky/pre-commit "npx bis --staged --react"
+
+# all other projects (ie backend)
+npx husky add .husky/pre-commit "npx bis --staged"
 ```
 
 - Add your own `.eslintignore` and `.prettierignore` files to the root of your project.
+
+# Extending eslint config
+
+We don't recommend extending the config, instead a PR should be made into this repo to add the change for everyone.
+
+If you need to extend the config, you can extend it like you would any other eslint config. The simplest way to do so can be seen in the `.eslintrc` file at the root of this repo.
+
+## What's next
+
+- try adding airbnb extends
+- setup commit validator & changelog generator
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,69 @@
 #!/usr/bin/env node
+import arg from 'arg';
+import { execSync } from 'child_process';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const lintStaged = require('lint-staged');
 
 (async () => {
-  const success = await lintStaged({
-    config: {
-      '*.{js,jsx,ts,tsx}': [
-        'npx prettier --config ./node_modules/prettier-config-bouncedinc/index.json --write',
-        'npx eslint -c ./node_modules/eslint-config-bouncedinc/index.js --cache --fix'
-      ]
-    }
+  const args = arg({
+    // Types
+    '--react': Boolean,
+    '--staged': Boolean,
+    '--help': Boolean,
+    '--check': Boolean,
+    // Aliases
+    '-h': '--help'
   });
 
-  console.log(success ? 'Format and lint was successful 🏀' : 'Formatting failed!');
+  if (args['--help']) {
+    console.log(`
+Usage
+  $ npx bis [flags]
+
+Options
+  --help        Print help message.
+  --react       Use for React & React Native projects.
+  --staged      Only run on staged files. Useful when adding as a git hook.
+  --check       Only check files. Skips running Prettier and doesn't use the --fix flag for ESLint. 
+                This option does not support the --staged flag.
+  `);
+    return;
+  }
+
+  const isStagedChanges = args['--staged'];
+  const isReact = args['--react'];
+  const isCheck = args['--check'];
+
+  if (isStagedChanges && isCheck) {
+    console.error('Cannot provide the --staged and the --check flag together');
+    return;
+  }
+
+  const prettierCommand =
+    'npx prettier --config ./node_modules/prettier-config-bouncedinc/index.json --write';
+  const eslintCommand = `npx eslint -c ./node_modules/eslint-config-bouncedinc${
+    isReact ? '-react' : ''
+  }/index.js ${isCheck ? '' : '--fix'} --cache --ext ts --ext tsx --ext js --ext jsx`;
+
+  if (isStagedChanges) {
+    await lintStaged({
+      config: {
+        '*.{js,jsx,ts,tsx}': [prettierCommand, eslintCommand],
+        '*.{json,css,md,yml,yaml}': [prettierCommand]
+      }
+    });
+  } else {
+    // append "." for files to format and lint
+    if (!isCheck) execSync(prettierCommand + ' .', { stdio: 'inherit' });
+
+    try {
+      // errors already get printed automatically, we dont need to
+      // print more
+      execSync(eslintCommand + ' .', { stdio: 'inherit' });
+    } catch (err) {
+      return;
+    }
+  }
+
+  console.log('Formating and linting was successful 🏀');
 })();
