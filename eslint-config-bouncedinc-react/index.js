15ec981caf30e0a0a04f98fadc14d27116fe3a98
Francesco Virga <francescod.virga@gmail.com>
2021-06-05 23:52:50 -0400

    Add react rules adn extends:

--- eslint-config-bouncedinc-react/index.js
+++ eslint-config-bouncedinc-react/index.js
@@ -1,7 +1,35 @@
 module.exports = {
-  extends: 'eslint-config-bouncedinc',
-  rules: {},
+  extends: ['eslint-config-bouncedinc', 'plugin:react/recommended'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_'
+      }
+    ],
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error']
+      }
+    ],
+    'no-case-declarations': 0
+  },
   env: {
-    browser: true
+    browser: true,
+    es6: true,
+    jest: true,
+    node: true
   }
 };
