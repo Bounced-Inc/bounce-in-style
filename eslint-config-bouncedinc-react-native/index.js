071837cc2f28613826d6c2aec66e371fe474d95d
Francesco Virga <francescod.virga@gmail.com>
2021-06-07 19:42:30 -0400

    Add todo

--- eslint-config-bouncedinc-react-native/index.js
+++ eslint-config-bouncedinc-react-native/index.js
@@ -4,3 +4,5 @@ module.exports = {
   rules: {},
   env: {}
 };
+
+// TODO lint: update this with new version of other two packages, weve stopped maintaining until we finish setting up the react web configs
