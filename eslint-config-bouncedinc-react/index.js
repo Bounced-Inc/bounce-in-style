c96852ce69ed9292b0ec1d1bcdf1158e51c517a0
Francesco Virga <francescod.virga@gmail.com>
2021-06-10 19:52:34 -0400

    Add more react rules

--- eslint-config-bouncedinc-react/index.js
+++ eslint-config-bouncedinc-react/index.js
@@ -6,6 +6,10 @@ module.exports = {
     'import/order': 'off',
     // TODO: this should be removed, but we have way too many prop destructures that do this
     'no-shadow': 'off',
+    // TODO: more that should be removed eventually
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/require-default-props': 'off',
 
     // TODO: properties that are also in base, find a way to not override these by accident
     // 'indent': 'off',
@@ -31,9 +35,25 @@ module.exports = {
     'react/jsx-curly-newline': 'off',
 
     'react/prop-types': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] }
+    ],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+    'jsx-a11y/alt-text': 'off',
+    'react/display-name': 'off',
     'import/no-named-as-default-member': 'off',
     '@typescript-eslint/explicit-member-accessibility': 0,
-    '@typescript-eslint/member-delimiter-style': 0
+    '@typescript-eslint/member-delimiter-style': 0,
+    'prefer-destructuring': 'off',
+    'jsx-a11y/media-has-caption': 'off',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/iframe-has-title': 'off',
+
+    // warnings
+    'react/no-did-update-set-state': 'warn',
+    'react/jsx-props-no-spreading': 'warn'
 
     // disabled rules from previous eslint web config
     // 'react/display-name': 0,
@@ -58,5 +78,43 @@ module.exports = {
     'import/resolver': {
       typescript: {} // this loads <rootdir>/tsconfig.json to eslint
     }
-  }
+  },
+  // rules that we turn off for old js code
+  overrides: [
+    {
+      files: ['**/*.js'],
+      rules: {
+        'react/forbid-prop-types': 'off',
+        'react/destructuring-assignment': 'off',
+        'react/require-default-props': 'off',
+        'react/no-array-index-key': 'off',
+        'react/sort-comp': 'off',
+        'no-nested-ternary': 'off',
+        'react/prefer-stateless-function': 'off',
+        'react/button-has-type': 'off',
+        'jsx-a11y/interactive-supports-focus': 'off',
+        'no-unused-expressions': 'off',
+        'jsx-a11y/label-has-associated-control': 'off',
+        // this is a copy of airbnbs version, with the ForOfStatement rule removed
+        'no-restricted-syntax': [
+          'error',
+          {
+            selector: 'ForInStatement',
+            message:
+              'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.'
+          },
+          {
+            selector: 'LabeledStatement',
+            message:
+              'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.'
+          },
+          {
+            selector: 'WithStatement',
+            message:
+              '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.'
+          }
+        ]
+      }
+    }
+  ]
 };
