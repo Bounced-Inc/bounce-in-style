b92ac45b55958f0f47e0b3ec5d0fa0530e4f2c48
Francesco Virga <francescod.virga@gmail.com>
2021-06-11 18:24:25 -0400

    Remove react native extends, add plugins

--- eslint-config-bouncedinc-react-native/index.js
+++ eslint-config-bouncedinc-react-native/index.js
@@ -1,8 +1,10 @@
 module.exports = {
-  root: true,
-  extends: ['eslint-config-bouncedinc-react', '@react-native-community'],
-  rules: {},
-  env: {}
+  // root: true,
+  // extends: ['@react-native-community', 'eslint-config-bouncedinc-react'],
+  extends: ['eslint-config-bouncedinc-react'],
+  plugins: ['react-native', '@react-native-community'],
+  rules: {}
+  // env: {}
 };
 
 // TODO lint: update this with new version of other two packages, weve stopped maintaining until we finish setting up the react web configs
