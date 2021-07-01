e113279721fbcf7d812a1e0c1ac4481fbee7953f
Francesco Virga <francescod.virga@gmail.com>
2021-06-30 20:11:20 -0400

    feat: add new rules, update packages

--- eslint-config-bouncedinc-react-native/index.js
+++ eslint-config-bouncedinc-react-native/index.js
@@ -6,7 +6,9 @@ module.exports = {
   rules: {
     'react/jsx-props-no-spreading': 'off',
     // NOTE: circular dep ignore will be removed in the near future
-    'import/no-cycle': 'off'
+    'import/no-cycle': 'off',
+    'react/no-unescaped-entities': 'off',
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }]
   }
   // env: {}
 };
